2ed7dc4630df8dd5f0f8153e4ba548471e8074cb
d.peters <d.peters@PC0185.Stiftung.ifa>
2017-01-04 16:31:06 +0100

    Added HashMap to contain Menu items and actionlist

--- src/postfixcalculator/CalculatorMenu.java
+++ src/postfixcalculator/CalculatorMenu.java
@@ -1,9 +1,22 @@
 package postfixcalculator;
 
+import java.util.HashMap;
+
 /**
  *
  * @author d.peters
  */
 public class CalculatorMenu {
     
+    /**
+     * 
+     */
+    private HashMap menuItems;
+    
+    /**
+     * Default constructor
+     */
+    public CalculatorMenu(){
+        this.menuItems = new HashMap();
+    }
 }
