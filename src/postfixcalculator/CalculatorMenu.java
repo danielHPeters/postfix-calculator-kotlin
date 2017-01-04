696c3deca51f4a531a1daae4a61a4e098d774064
d.peters <d.peters@PC0185.Stiftung.ifa>
2017-01-04 16:53:12 +0100

    Added more comments

--- src/postfixcalculator/CalculatorMenu.java
+++ src/postfixcalculator/CalculatorMenu.java
@@ -3,18 +3,19 @@ package postfixcalculator;
 import java.util.HashMap;
 
 /**
- *
+ * Menu of the Postfix calculator
  * @author d.peters
  */
 public class CalculatorMenu {
     
     /**
-     * 
+     * The Menuitems
      */
     private HashMap menuItems;
     
     /**
-     * Default constructor
+     * Default constructor.
+     * Initializes menu items
      */
     public CalculatorMenu(){
         this.menuItems = new HashMap();
